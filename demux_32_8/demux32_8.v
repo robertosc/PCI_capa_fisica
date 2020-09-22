99da5f786255393fabc16f697c39abb1ea28c539
robertosc <robert.s.c30@gmail.com>
2020-09-22 11:15:44 -0600

    se añade mux 32:8 v2

--- demux_32_8/demux32_8.v
+++ demux_32_8/demux32_8.v
@@ -6,7 +6,6 @@ module demux32_8(output reg [7:0] data_out,
                 input valid_0);
 
     reg [1:0] counter1;
-
     always @(posedge clk_4f)begin
         if(reset==0)begin
             counter1[1:0]<=2'b00;
@@ -14,34 +13,34 @@ module demux32_8(output reg [7:0] data_out,
         end
         else begin
             if(valid_0 == 1)begin
-                if(counter1 == 2'b00) begin
+                if(counter1[1:0] == 2'b00) begin
                     data_out <= lane_0[31:24];
                     valid_out <= valid_0;
-                    counter1 <= 2'b01;
+                    counter1[1:0] <= 2'b01;
                 end
 
-                else if(counter1 == 2'b01) begin
+                else if(counter1[1:0] == 2'b01) begin
                     data_out <= lane_0[23:16];
                     valid_out <= valid_0;
-                    counter1 <= 2'b10;
+                    counter1[1:0] <= 2'b10;
                 end
 
-                else if(counter1 == 2'b10) begin
+                else if(counter1[1:0] == 2'b10) begin
                     data_out <= lane_0[15:8];
                     valid_out <= valid_0;
-                    counter1 <= 2'b11;
+                    counter1[1:0] <= 2'b11;
                 end
 
-                else if(counter1 == 2'b11) begin
+                else if(counter1[1:0] == 2'b11) begin
                     data_out <= lane_0[7:0];
                     valid_out <= valid_0;
-                    counter1 <= 2'b00;
+                    counter1[1:0] <= 2'b00;
                 end
             end
 
             else begin
                 valid_out <=0;
-                counter1 <=2'b00;           
+                counter1[1:0] <=2'b00;           
             end
         end    
 
---
+++ demux_32_8_v2/probador_demux_32_8.v
@@ -0,0 +1,64 @@
+module probador_demux_32_8(input [7:0] data_out,
+						input [7:0] data_out_estructural,
+						input valid_out_estructural,
+						input valid_out,
+						output reg valid,
+						output reg [31:0] data_in,
+						output reg clk_4f,
+						output reg clk_2f,
+						output reg clk_f,
+						output reg reset);
+	initial begin
+	$dumpfile("demux_32_8.vcd");
+	$dumpvars;
+
+	{valid, reset} <= 0;
+	{data_in} <= 32'h00EEBB33;
+
+	
+	@(posedge clk_f);
+	reset <= 1;
+	valid <= 1;
+	data_in <= 32'hEEFF9900;
+
+	@(posedge clk_f);
+	valid <= 1;
+	data_in <= 32'hFFAABB22;
+
+	@(posedge clk_f);
+	valid <= 1;
+	data_in <= 32'hFD554488;
+
+	@(posedge clk_f);
+	valid <= 1;
+	data_in <= 32'hCCBBAAFF;
+
+	@(posedge clk_f);
+	valid <= 1;
+	data_in <= 32'hFD554488;
+
+	@(posedge clk_f);
+	valid <= 1;
+	data_in <= 32'h12345678;
+
+	@(posedge clk_f);
+	valid <= 1;
+	data_in <= 32'hBBCCDDEE;
+
+	@(posedge clk_f);
+
+	@(posedge clk_f);
+	$finish;
+	end
+	
+	initial clk_4f <= 0;
+	always #1 clk_4f <= ~clk_4f;
+
+	initial clk_2f <= 0;
+	always #2 clk_2f <= ~clk_2f;
+
+	initial clk_f <= 0;
+	always #4 clk_f <= ~clk_f;
+
+
+endmodule
