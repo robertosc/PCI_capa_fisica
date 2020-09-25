6f5011a329a3989b1dbee160efb554e838f54f2c
jose-543 <josepablocc543@gmail.com>
2020-09-24 23:29:26 -0600

    paraleloserial

---
+++ paralelo_serial/banco_paralelo_serial.v
@@ -0,0 +1,43 @@
+`timescale 1ms/100ps
+
+`include "paralelo_serial.v"
+`include "probador_paralelo_serial.v"
+`include "sintetizado_paralelo_serial_cmos.v"
+
+    module banco_paralelo_serial;
+    	wire [7:0] data2send, data_in, data2send_estructural;
+    	wire clk_4f, clk_32f, valid_in, data_out, data_out_estructural;
+
+    	paralelo_serial p_s(/*AUTOINST*/
+			    // Outputs
+			    .data2send		(data2send[7:0]),
+			    .data_out		(data_out),
+			    // Inputs
+			    .clk_4f		(clk_4f),
+			    .clk_32f		(clk_32f),
+			    .data_in		(data_in[7:0]),
+			    .valid_in		(valid_in));
+		sintetizado_paralelo_serial_cmos s_p_s(/*AUTOINST*/
+				// Outputs
+				.data2send_estructural(data2send_estructural[7:0]),
+				.data_out_estructural(data_out_estructural),
+				// Inputs
+				.clk_32f		(clk_32f),
+				.clk_4f		(clk_4f),
+				.data_in		(data_in[7:0]),
+				.valid_in	(valid_in));
+
+
+    	probador_paralelo_serial prob_paralelo_serial(/*AUTOINST*/
+						      // Outputs
+						      .valid_in		(valid_in),
+						      .data_in		(data_in[7:0]),
+						      .clk_4f		(clk_4f),
+						      .clk_32f		(clk_32f),
+						      // Inputs
+						      .data2send	(data2send[7:0]),
+						      .data2send_estructural(data2send_estructural[7:0]),
+						      .data_out		(data_out),
+						      .data_out_estructural(data_out_estructural));
+
+    endmodule
---
+++ paralelo_serial/paralelo_serial.v
@@ -0,0 +1,90 @@
+module paralelo_serial(input clk_4f,
+                        input clk_32f,
+                        input [7:0] data_in,
+                        input valid_in,
+                        output reg [7:0] data2send,
+                        output reg data_out);
+
+    reg [2:0] selector = 3'b000;
+
+    always @(posedge clk_4f) begin
+        if (valid_in==0)begin
+            data2send <= 8'hBC;
+            selector [2:0] <= 3'b000;          
+        end
+        else if (valid_in==1) begin
+            data2send <= data_in;       
+        end
+    end
+
+    always @(posedge clk_32f) begin
+        if (valid_in == 1)begin
+            if (selector==3'b000) begin
+                data_out <= data_in[7];
+                selector <= 3'b001;
+            end
+            else if (selector==3'b001) begin
+                data_out <= data_in[6];
+                selector <= 3'b011;
+            end            
+            else if (selector==3'b011) begin
+                data_out <= data_in[5];
+                selector <= 3'b111;
+            end
+            else if (selector==3'b111) begin
+                data_out <= data_in[4];
+                selector <= 3'b110;
+            end 
+            else if (selector==3'b110) begin
+                data_out <= data_in[3];
+                selector <= 3'b100;
+            end 
+            else if (selector==3'b100) begin
+                data_out <= data_in[2];
+                selector <= 3'b101;
+            end 
+            else if (selector==3'b101) begin
+                data_out <= data_in[1];
+                selector <= 3'b010;
+            end 
+            else if (selector==3'b010) begin
+                data_out <= data_in[0];
+                selector <= 3'b000;
+            end 
+        end
+        else begin
+            if (selector==3'b000) begin
+                data_out <= 1;
+                selector <= 3'b001;
+            end
+            else if (selector==3'b001) begin
+                data_out <= 0;
+                selector <= 3'b011;
+            end            
+            else if (selector==3'b011) begin
+                data_out <= 1;
+                selector <= 3'b111;
+            end
+            else if (selector==3'b111) begin
+                data_out <= 1;
+                selector <= 3'b110;
+            end 
+            else if (selector==3'b110) begin
+                data_out <= 1;
+                selector <= 3'b100;
+            end 
+            else if (selector==3'b100) begin
+                data_out <= 1;
+                selector <= 3'b101;
+            end 
+            else if (selector==3'b101) begin
+                data_out <= 0;
+                selector <= 3'b010;
+            end 
+            else if (selector==3'b010) begin
+                data_out <= 0;
+                selector <= 3'b000;
+            end
+        end
+    end
+endmodule
