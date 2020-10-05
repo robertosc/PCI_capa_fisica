bbd2d1007edfb8e512dc367d63df0045837574b4
albertomata8 <betomata800@gmail.com>
2020-10-05 00:16:46 -0600

    ya no c que poner aca

--- demux_8_32/demux_8_32.v
+++ demux_8_32/demux_8_32.v
@@ -16,32 +16,45 @@ module demux_8_32(input clk_4f,
 			data_out <= 0;
 		end
 		else begin
+			if (data_out[31:0]!=8'h00000000) begin
+				valid_out <= 1;
+			end
+			else begin
+				valid_out <= 0;
+			end
+			if (transicion[31:0]!=8'h00000000) begin
+				if (selector==2'b00) begin
+					valid_out <= 1;
+				end
+			end
+			else if (transicion[31:0]==8'h00000000) begin
+				if (selector==2'b00) begin
+					valid_out <= 0;
+				end
+			end
 			if(valid == 1) begin
 				if(selector == 2'b00) begin
 					transicion [31:24] <= data_in;
-					valid_out <= valid;
 					selector <= 2'b01;
 					data_out[31:0]<=transicion[31:0];
 				end
 				else if(selector == 2'b01) begin
 					transicion [23:16] <= data_in;
-					valid_out <= 0;
 					selector <= 2'b10;
 				end
 				else if(selector[1] == 1 && selector[0] == 0) begin
 					transicion [15:8] <= data_in;
-					valid_out <= 0;
 					selector <= 2'b11;
 				end
 				else if(selector == 2'b11) begin
 					transicion [7:0] <= data_in;
-					valid_out <= valid;
 					selector <= 2'b00;
+					
+				
 				end
 			end
 			else begin
 				data_out[31:0]<=transicion[31:0];
-				valid_out <= 0;
 				transicion <=0;
 				selector <= 2'b00;
 			end
