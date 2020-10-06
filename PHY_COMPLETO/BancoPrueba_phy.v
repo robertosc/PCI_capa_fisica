deb110b1bd7bbe6af84516d57d1ea5bc71759f42
albertomata8 <betomata800@gmail.com>
2020-10-06 00:01:42 -0600

    final

--- PHY_COMPLETO/BancoPrueba_phy.v
+++ PHY_COMPLETO/BancoPrueba_phy.v
@@ -1,10 +1,11 @@
 `include "probador_phy.v"
 `include "phy.v"
+`include "PHY_COMPLETO_sintetizado.v"
 //`include "phy_sintetizado.v"
 module BancoPrubas_PHY;
 
-	wire [31:0] data_input, data_final;
-	wire valid_final;
+	wire [31:0] data_input, data_final, data_final_sintetizado;
+	wire valid_final, valid_final_sintetizado;
    phy completo(/*AUTOINST*/
 		// Outputs
 		.data_final		(data_final[31:0]),
@@ -18,7 +19,19 @@ module BancoPrubas_PHY;
 		.clk_f			(clk_f),
 		.clk_4f			(clk_4f),
 		.clk_32f		(clk_32f));
-   
+	PHY_COMPLETO_sintetizado sintetizado(/*AUTOINST*/
+		// Outputs
+		.data_final_sintetizado		(data_final_sintetizado[31:0]),
+		.valid_final_sintetizado		(valid_final_sintetizado),
+		// Inputs
+		.data_input		(data_input[31:0]),
+		.valid			(valid),
+		//.active			(active),
+		.reset			(reset),
+		.clk_2f			(clk_2f),
+		.clk_f			(clk_f),
+		.clk_4f			(clk_4f),
+		.clk_32f		(clk_32f));
 
 
 
