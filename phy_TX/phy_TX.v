82c607bc58411bbd21fcaa4dc1f5784cc0684cf3
robertosc <robert.s.c30@gmail.com>
2020-10-06 22:38:58 -0600

    .

--- phy_TX/phy_TX.v
+++ phy_TX/phy_TX.v
@@ -22,27 +22,73 @@ module phy_TX(input [31:0] data_input,
 
 	recirculador recirculador_inicial(
 		/*AUTOINST*/
-	);
+					  // Outputs
+					  .data_recirculador_active(data_recirculador_active[31:0]),
+					  .data_recirculador_inactive(data_recirculador_inactive[31:0]),
+					  .valid_recirculador	(valid_recirculador),
+					  // Inputs
+					  .clk_2f		(clk_2f),
+					  .data_input		(data_input[31:0]),
+					  .valid		(valid),
+					  .data_and_active	(data_and_active),
+					  .reset		(reset));
 
 	demux_striping u_demux_striping(
 		/*AUTOINST*/
-	);
+					// Outputs
+					.data_demux_strp_0(data_demux_strp_0[31:0]),
+					.data_demux_strp_1(data_demux_strp_1[31:0]),
+					.valid_demux_strp_0(valid_demux_strp_0),
+					.valid_demux_strp_1(valid_demux_strp_1),
+					// Inputs
+					.clk_2f		(clk_2f),
+					.data_recirculador_active(data_recirculador_active[31:0]),
+					.valid_recirculador(valid_recirculador),
+					.reset		(reset));
 
 	demux_32_8 demux_lane_0(
 		/*AUTOINST*/
-	);
+				// Outputs
+				.data_demux_32_8(data_demux_32_8[7:0]),
+				.valid_demux_32_8(valid_demux_32_8),
+				// Inputs
+				.clk_4f		(clk_4f),
+				.data_demux_strp(data_demux_strp[31:0]),
+				.valid_demux_strp(valid_demux_strp),
+				.reset		(reset));
 
 	paralelo_serial paralelo_serial0(
 		/*AUTOINST*/
-	);
+					 // Outputs
+					 .data_paralelo_serial	(data_paralelo_serial),
+					 // Inputs
+					 .clk_4f		(clk_4f),
+					 .clk_32f		(clk_32f),
+					 .data_demux_32_8	(data_demux_32_8[7:0]),
+					 .valid_in		(valid_in),
+					 .reset			(reset));
 	
 	demux_32_8 demux_lane_1(
 		/*AUTOINST*/
-	);
+				// Outputs
+				.data_demux_32_8(data_demux_32_8[7:0]),
+				.valid_demux_32_8(valid_demux_32_8),
+				// Inputs
+				.clk_4f		(clk_4f),
+				.data_demux_strp(data_demux_strp[31:0]),
+				.valid_demux_strp(valid_demux_strp),
+				.reset		(reset));
 
 	paralelo_serial paralelo_serial1(
 		/*AUTOINST*/
-	);
+					 // Outputs
+					 .data_paralelo_serial	(data_paralelo_serial),
+					 // Inputs
+					 .clk_4f		(clk_4f),
+					 .clk_32f		(clk_32f),
+					 .data_demux_32_8	(data_demux_32_8[7:0]),
+					 .valid_in		(valid_in),
+					 .reset			(reset));
 
 
 
