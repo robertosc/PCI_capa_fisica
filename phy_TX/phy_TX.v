fa9f98677e7271844640eabb1f61a6e83c80836d
albertomata8 <betomata800@gmail.com>
2020-10-06 22:34:46 -0600

    hola

--- phy_TX/phy_TX.v
+++ phy_TX/phy_TX.v
@@ -21,61 +21,27 @@ module phy_TX(input [31:0] data_input,
 	wire valid_demux_striping_0, valid_demux_striping_1, valid_out0, valid_out1;
 
 	recirculador recirculador_inicial(
-		.clk_2f           ( clk_2f           ),
-		.data_input       ( data_input [31:0]),
-		.valid            ( valid            ),
-		.active           ( active           ),
-		.demux_0          ( demux_0  [31:0]  ),
-		.reset      ( reset      ),
-		.demux_1_probador ( demux_1_probador[31:0]),
-		.valid_out        ( valid_out        )
+		/*AUTOINST*/
 	);
 
 	demux_striping u_demux_striping(
-		.clk_2f     ( clk_2f     ),
-		.data_input ( demux_0[31:0]	),
-		.valid_in   ( valid_out  ),
-		.reset      ( reset      ),
-		.lane_0     ( lane_0 [31:0]),
-		.lane_1     ( lane_1 [31:0]),
-		.valid_out0 ( valid_demux_striping_0 ),
-		.valid_out1 ( valid_demux_striping_1  )
+		/*AUTOINST*/
 	);
 
 	demux_32_8 demux_lane_0(
-		.data_out  ( demux0_paraleloserial [7:0]),
-		.valid_out ( valid_out0 ),
-		.reset     ( reset     ),
-		.clk_4f    ( clk_4f    ),
-		.data_in    ( lane_0[31:0]    ),
-		.valid   ( valid_demux_striping_0 )
+		/*AUTOINST*/
 	);
 
 	paralelo_serial paralelo_serial0(
-		.clk_4f 	(	clk_4f	),
-		.clk_32f	(	clk_32f	),
-		.data_in	( demux0_paraleloserial [7:0] ),
-		.valid_in 	( valid_out0 ),
-		.reset 		( reset ),
-		.data_out	( data_out_ps0 )
+		/*AUTOINST*/
 	);
 	
 	demux_32_8 demux_lane_1(
-		.data_out  ( demux1_paraleloserial [7:0]),
-		.valid_out ( valid_out1 ),
-		.reset     ( reset     ),
-		.clk_4f    ( clk_4f    ),
-		.data_in    ( lane_1[31:0]    ),
-		.valid   ( valid_demux_striping_1 )
+		/*AUTOINST*/
 	);
 
 	paralelo_serial paralelo_serial1(
-		.clk_4f 	(	clk_4f	),
-		.clk_32f	(	clk_32f	),
-		.data_in	( demux1_paraleloserial [7:0] ),
-		.valid_in 	( valid_out1 ),
-		.reset 		( reset ),
-		.data_out	( data_out_ps1 )
+		/*AUTOINST*/
 	);
 
 
